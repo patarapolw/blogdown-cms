da91a221c6bf4870e981c93f72240178616aa852
Pacharapol Withayasakpunt <patarapolw@gmail.com>
2020-01-01 04:51:52 +0300

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'standard',
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    parser: '@typescript-eslint/parser',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'no-unused-vars': 0,
+    'no-useless-constructor': 0,
+    'no-cond-assign': 0,
+    'no-undef': 0,
+    'no-new': 0,
+    'quote-props': ['error', 'as-needed'],
+    'comma-dangle': ['error', 'always-multiline'],
+    semi: 'off',
+    '@typescript-eslint/semi': ['error', 'never'],
+    '@typescript-eslint/member-delimiter-style': ['error', {
+      multiline: {
+        delimiter: 'none',
+      },
+    }],
+  },
+}
